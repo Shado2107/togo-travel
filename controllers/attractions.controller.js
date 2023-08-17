9588477363811d2ee3cfaaac060436ccd9603fe1
Shado2107 <pd39almeida@outlook.com>
2023-08-17 17:57:12 +0000

    attractions controller begiining

--- controllers/attractions.controller.js
+++ controllers/attractions.controller.js
@@ -2,11 +2,32 @@ const AttractionModel = require('../models/attractions.model')
 
 
 module.exports.getAttraction = async(req, res) => {
-    
+    const attraction = await AttractionModel.find({}).populate('city', 'name');
+
+    res.status(200).json({succes: true, attration: attraction })
 }
 
 module.exports.addAttraction = async(req, res) => {
-    
+    try{
+        const {city, name, category, description} = req.body;
+
+        if(!city){
+            return res.status(400).json({success: false, message: "veuiller ajouter la ville de l\'attraction"})
+        }
+
+        const newAttraction = await new AttractionModel({
+            city,
+            name,
+            category,
+            description
+        });
+
+        const savedAttraction = await newAttraction.save();
+        res.status(201).json({success: true, message: "attraction sauvegardé avec sucess", savedAttraction})
+
+    } catch(err){
+        res.staus(500).json({succes: false, message:"Une erreur est surevenue"})
+    }
 }
 
 module.exports.editAttraction = async(req, res) => {
--- controllers/city.controller.js
+++ controllers/city.controller.js
@@ -2,7 +2,7 @@ const cityModel = require("../models/city.model")
 
 module.exports.getCity = async(req, res) => {
     const city = await cityModel.find({});
-    res.status(200).json({success: true, city});
+    res.status(200).json({success: true, city: city});
 }
 
 module.exports.addCity = async(req, res) => {
